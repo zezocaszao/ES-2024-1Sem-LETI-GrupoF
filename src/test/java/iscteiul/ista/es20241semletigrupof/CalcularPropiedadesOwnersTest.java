ab1374db774eca3a299e749820bcb235c627cb23
marcos_fig <mfdfo>
2024-12-01 16:30:17 +0000

    teste adicionados para algumas classes

---
+++ src/test/java/iscteiul/ista/es20241semletigrupof/CalcularPropiedadesOwnersTest.java
@@ -0,0 +1,24 @@
+package iscteiul.ista.es20241semletigrupof;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcularPropiedadesOwnersTest {
+
+    @Test
+    void sameOwnership() {
+    }
+
+    @Test
+    void propsNotNeighor() {
+    }
+
+    @Test
+    void contarPropriedadesComVizinhosMesmoOwner() {
+    }
+
+    @Test
+    void averageAreaOwner() {
+    }
+}
